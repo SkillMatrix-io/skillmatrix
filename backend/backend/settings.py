97b8fc4e3b0e437fea6a3bfbde158c270073ce10
Vineet Kumar Kushwaha <vineetkushwaha6325@gmail.com>
2025-08-09 14:09:32 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders' #allowing origins
+    'corsheaders', #allowing origins
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
